1280e6c45d553a9fb0a7e55e97bc3b0d39148da4
zkanavi <avinash.kandagatla-external@tcs.com>
2018-11-19 16:01:23 +0530

    Second exapmle

---
+++ example3.py
@@ -0,0 +1,23 @@
+from threading import Thread
+import threading
+from time import sleep
+
+def target_fun(i):
+    print(i + "  target function")
+    print(threading.active_count())
+    sleep(3)
+    print(i + "  task completed")
+    print(threading.active_count())
+
+
+l = ['abc','def','xyz','ramu','somu','ravi','ajay']
+for i in l:
+    t1 = Thread(target=target_fun,args=(i,))
+    #t1.run()     # Which takes considerable time
+    t1.start()   # With help of threading , executio will be faster
+
+
+
+
+
+
